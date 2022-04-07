de4419fb4819cb407d31f611cba49be569cb7b4c
Agustín Vargas <agustinvargas93@gmail.com>
2022-04-07 17:31:14 -0300

    add status button

---
+++ src/components/Buttons/StatusButton.js
@@ -0,0 +1,22 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+const StatusButton = ({ status }) => {
+  return (
+    <Button
+      sx={{
+        background: '#F5F5F5',
+        border: '1px solid #D9D9D9',
+        borderRadius: '4px',
+        padding: '1px 8px',
+        color: '#595959',
+        fontSize: '12px',
+      }}
+      variant='outlined'
+    >
+      {status ? 'Enabled' : 'Disabled'}
+    </Button>
+  );
+};
+
+export default StatusButton;
